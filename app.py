dadf14f97fcfd0c9e61b4e9351a9f49e5ebbb1be
Agnieszka Szefer <agnieszka@insidevault.com>
2015-07-18 21:48:15 -0700

    Working ocr request.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from flask import render_template
+from flask import Flask
+from flask import render_template, redirect
 
 app = Flask(__name__)
 
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/ocr')
+def ocr():
+    return render_template('ocr.html')
 
 if __name__ == '__main__':
     app.run()
