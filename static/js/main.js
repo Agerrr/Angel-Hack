f61f49ac6515237932fc135a6eec732cc208632f
Haochi Chen <haochi.chen@gmail.com>
2015-07-18 18:32:44 -0700

    Working brushes selection

--- static/js/main.js
+++ static/js/main.js
@@ -1,101 +1,90 @@
 angular
     .module("fazer", [])
     .controller("FazerCtrl", function () {
-        this.brushes = [];
+        var canvas;
+        var context;
+        var stage;
+        var drawing;
+        var lastPoint;
+        var me = this;
+
+        this.brushes = [
+            { name: 'Normal', fn: brushes.normal },
+            { name: 'Fancy', fn: brushes.orange },
+        ];
+
+        this.brush = this.brushes[0];
+
+        this.selectBrush = function (brush) {
+            this.brush = brush;
+        }
+
+        this.init = function () {
+            canvas = document.getElementById("primary");
+            ctx = canvas.getContext('2d');
+
+            stage = new createjs.Stage(canvas);
+            stage.autoClear = false;
+            stage.enableDOMEvents(true);
+
+            createjs.Touch.enable(stage);
+            createjs.Ticker.setFPS(24);
+
+            stage.addEventListener("stagemousedown", handleMouseDown);
+            stage.addEventListener("stagemouseup", handleMouseUp);
+            stage.addEventListener("stagemousemove", handleMouseMove);
+
+            stage.addChild(new createjs.Shape());
+            stage.update();
+        }
+
+        this.init();
+
+        function handleMouseDown(event) {
+            if (!event.primary) { return; }
+            drawing = true;
+            lastPoint = { x: stage.mouseX, y: stage.mouseY };
+        }
+
+        function handleMouseMove(event) {
+            if (!event.primary) { return; }
+            if (!drawing) { return; }
+
+            var currentPoint = { x: stage.mouseX, y: stage.mouseY };
+            me.brush.fn(ctx, lastPoint, currentPoint);
+            lastPoint = currentPoint;
+        }
+
+        function handleMouseUp(event) {
+            if (!event.primary) { return; }
+            drawing = false;
+        }
     });
 
 
-var canvas, stage;
-var drawingCanvas;
-var drawing;
 
-function init() {
-    canvas = document.getElementById("primary");
+var brushes = {
+    normal: function normal(ctx, lastPoint, currentPoint) {
+        ctx.moveTo(lastPoint.x, lastPoint.y);
+        ctx.lineTo(currentPoint.x, currentPoint.y);
+        ctx.stroke();
+    },
+    fancy: function fancy(ctx, lastPoint, currentPoint) {
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        ctx.beginPath();
 
-    //check to see if we are running in a browser with touch support
-    stage = new createjs.Stage(canvas);
-    stage.autoClear = false;
-    stage.enableDOMEvents(true);
+        ctx.moveTo(lastPoint.x - getRandomInt(0, 2), lastPoint.y - getRandomInt(0, 2));
+        ctx.lineTo(currentPoint.x - getRandomInt(0, 2), currentPoint.y - getRandomInt(0, 2));
+        ctx.stroke();
 
-    createjs.Touch.enable(stage);
-    createjs.Ticker.setFPS(24);
+        ctx.moveTo(lastPoint.x, lastPoint.y);
+        ctx.lineTo(currentPoint.x, currentPoint.y);
+        ctx.stroke();
 
-    drawingCanvas = new createjs.Shape();
-
-    stage.addEventListener("stagemousedown", handleMouseDown);
-    stage.addEventListener("stagemouseup", handleMouseUp);
-    stage.addEventListener("stagemousemove", handleMouseMove);
-
-    stage.addChild(drawingCanvas);
-    stage.update();
-}
-
-function handleMouseDown(event) {
-    if (!event.primary) {
-        return;
+        ctx.moveTo(lastPoint.x + getRandomInt(0, 2), lastPoint.y + getRandomInt(0, 2));
+        ctx.lineTo(currentPoint.x + getRandomInt(0, 2), currentPoint.y + getRandomInt(0, 2));
+        ctx.stroke();
     }
-    drawing = true;
-
-    lastPoint = {
-        x: stage.mouseX,
-        y: stage.mouseY
-    };
-}
-
-var normalBrush = false;
-var lastPoint = {};
-
-function handleMouseMove(event) {
-    if (!event.primary) {
-        return;
-    }
-    if (!drawing) {
-        return;
-    }
-
-    var currentPoint = {
-        x: stage.mouseX,
-        y: stage.mouseY
-    };
-    var ctx = drawingCanvas.stage.canvas.getContext('2d');
-    if (normalBrush) {
-        regularBrush(ctx, currentPoint)
-    } else {
-        orange(ctx, currentPoint);
-    }
-    lastPoint = currentPoint;
-}
-
-function handleMouseUp(event) {
-    if (!event.primary) {
-        return;
-    }
-    drawing = false;
-}
-
-function regularBrush(ctx, currentPoint) {
-    ctx.moveTo(lastPoint.x, lastPoint.y);
-    ctx.lineTo(currentPoint.x, currentPoint.y);
-    ctx.stroke();
-}
-
-function orange(ctx, currentPoint) {
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    ctx.beginPath();
-
-    ctx.moveTo(lastPoint.x - getRandomInt(0, 2), lastPoint.y - getRandomInt(0, 2));
-    ctx.lineTo(currentPoint.x - getRandomInt(0, 2), currentPoint.y - getRandomInt(0, 2));
-    ctx.stroke();
-
-    ctx.moveTo(lastPoint.x, lastPoint.y);
-    ctx.lineTo(currentPoint.x, currentPoint.y);
-    ctx.stroke();
-
-    ctx.moveTo(lastPoint.x + getRandomInt(0, 2), lastPoint.y + getRandomInt(0, 2));
-    ctx.lineTo(currentPoint.x + getRandomInt(0, 2), currentPoint.y + getRandomInt(0, 2));
-    ctx.stroke();
-}
-
-init();
+};
